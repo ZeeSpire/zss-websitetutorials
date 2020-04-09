de5c9e30784b1540c98cf60cbbb5d0c5016e691d
Gabriel Voicu <gabriel.voicu@dell.com>
2020-04-09 16:07:02 +0300

    fixing import

--- hexagonal-architecture/src/main/java/com/eskavision/hexagonalarchitecture/database/jparepository/HAAJpaRepository.java
+++ hexagonal-architecture/src/main/java/com/eskavision/hexagonalarchitecture/database/jparepository/HAAJpaRepository.java
@@ -1,6 +1,7 @@
 package com.eskavision.hexagonalarchitecture.database.jparepository;
 
 import com.eskavision.hexagonalarchitecture.database.domain.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HAAJpaRepository extends org.springframework.data.jpa.repository.JpaRepository<BookEntity, Long> {
+public interface HAAJpaRepository extends JpaRepository<BookEntity, Long> {
 }
