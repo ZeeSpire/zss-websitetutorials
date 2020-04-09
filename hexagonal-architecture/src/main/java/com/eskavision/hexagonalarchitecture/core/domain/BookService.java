cb9ef7e427c9ddaa7c73ed29f719e0ee7340328f
Gabriel Voicu <gabriel.voicu@dell.com>
2020-04-09 16:08:10 +0300

    fixing import

--- hexagonal-architecture/src/main/java/com/eskavision/hexagonalarchitecture/core/domain/BookService.java
+++ hexagonal-architecture/src/main/java/com/eskavision/hexagonalarchitecture/core/domain/BookService.java
@@ -1,7 +1,5 @@
 package com.eskavision.hexagonalarchitecture.core.domain;
 
-import com.eskavision.hexagonalarchitecture.core.domain.Book;
-
 public interface BookService {
     Book getBook(Long id);
 }
